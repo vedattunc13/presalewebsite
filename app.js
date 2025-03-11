dd56eaaa1fce01b6b4524bd6e98685d8fdc801d1
vedattunc13 <60415131+vedattunc13@users.noreply.github.com>
2025-03-11 22:40:42 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,5 @@
+<script src="https://cdnjs.cloudflare.com/ajax/libs/@solana/web3.js/1.31.0/web3.min.js"></script>
+<script src="https://unpkg.com/@solana/wallet-adapter-wallets"></script>
+<script src="https://unpkg.com/@solana/wallet-adapter-react"></script>
+<script src="https://unpkg.com/@solana/wallet-adapter-base"></script>
+<script src="https://unpkg.com/@solana/wallet-adapter-react-ui"></script>
